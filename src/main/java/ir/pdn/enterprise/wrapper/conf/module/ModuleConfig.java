bfd033aa769d7a52463c5ad02eb10821069b9ac8
mahdi <mahdi-pc@hotmail.com>
2023-03-04 12:53:22 +0330

    test

--- src/main/java/ir/pdn/enterprise/wrapper/conf/module/ModuleConfig.java
+++ src/main/java/ir/pdn/enterprise/wrapper/conf/module/ModuleConfig.java
@@ -1,6 +1,6 @@
 package ir.pdn.enterprise.wrapper.conf.module;
 
-import ir.pdn.enterprise.features.meeting.EnableMeetingModuleModule;
+import ir.pdn.enterprise.features.meeting.EnableMeetingModule;
 import ir.pdn.portal.core.module.EnablePortalCoreModule;
 import ir.pdn.portal.mirrorasset.EnableAssetMirrorModule;
 import ir.pdn.portal.security.module.EnablePortalSecurityModule;
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @EnablePortalCoreModule
 @EnablePortalSecurityModule
 @EnableAssetMirrorModule
-@EnableMeetingModuleModule
+@EnableMeetingModule
 public class ModuleConfig
 {
 
